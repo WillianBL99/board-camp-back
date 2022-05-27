56f8b88bc879166506774ed9a329e03782d2c5a9
paulo uilian <uilianlbarros@gmail.com>
2022-05-27 20:56:07 -0300

    Implementará get da rota rentalsRoute.js

--- src/routes/rentalsRoute.js
+++ src/routes/rentalsRoute.js
@@ -1,12 +1,47 @@
 import express from "express";
 import dotenv from "dotenv";
+import connection from "../database/db.js";
 
 dotenv.config();
 
 const rentalsRoute = express.Router();
 
-rentalsRoute.get('/rentals', (req, res) => {
+rentalsRoute.get('/rentals', async (req, res) => {
+  try {
+    const rentals = await connection.query(`
+      SELECT * FROM rentals
+    `);
 
+    
+
+    const result = [];
+    //console.log(rentals.rows, customer.rows, game.rows);
+    for(const rental of rentals.rows){
+
+      const {customerId, gameId} = rental;
+
+      const customer = await connection.query(`
+        SELECT id, name FROM customers
+        WHERE id=$1
+      `,[customerId]);
+
+      const game = await connection.query(`
+        SELECT gm.id, gm.name, gm."categoryId", cat.name AS "categoryName" 
+        FROM games gm
+        JOIN categories cat ON gm."categoryId" = cat.id
+        WHERE gm.id=$1
+      `,[gameId]);
+
+      result.push({...rental, customer: customer.rows[0], game: game.rows[0]})
+    }
+    console.log('result',result)
+   
+   res.send(result); 
+
+  } catch(e){
+    console.log("Error get customers.", e);
+    return res.sendStatus(500);
+  }
 });
 
 rentalsRoute.post('/rentals/:id',(req, res) => {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import gamesRoute from "./gamesRoute.js";
 import categoriesRoute from "./categoriesRoute.js";
 import customersRoute from "./customersRoute.js";
+import rentalsRoute from "./rentalsRoute.js";
 
 const routes = express.Router();
 routes.use(gamesRoute);
 routes.use(categoriesRoute);
 routes.use(customersRoute);
+routes.use(rentalsRoute);
 
 export default routes;
