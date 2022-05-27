6bfca25d5a842e19f7b098eaab0b89ce73b3c80a
paulo uilian <uilianlbarros@gmail.com>
2022-05-27 13:43:13 -0300

    Criará conexão com o banco

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(json());
