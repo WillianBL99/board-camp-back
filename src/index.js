33245df52652d4939afeaabbdb16f761f2004106
paulo uilian <uilianlbarros@gmail.com>
2022-05-27 11:49:58 -0300

    Primeiro commit - estrutura base

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express,{json} from "express";
+import chalk from "chalk";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.listen(port, () => {
+  console.log(chalk.blue(`Server is starting on port ${chalk.bold(port)}`));
+})
---
+++ src/routes/rentalsRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const rentalsRoute = express.Router();
+
+rentalsRoute.get('')
+
+export default rentalsRoute;
